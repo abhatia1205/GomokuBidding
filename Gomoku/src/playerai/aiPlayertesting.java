a0154633ea0784db266b254edc8a707047a780ac
Ndevrak <ndevrak@gmail.com>
2020-05-24 20:10:08 -0700

    i dont really know

--- Gomoku/src/playerai/aiPlayertesting.java
+++ Gomoku/src/playerai/aiPlayertesting.java
@@ -6,15 +6,11 @@ import java.util.*;
 
 public class aiPlayertesting {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        String n = sc.nextLine();
-        while(! n.equals("e")){
-            GomokuBoard b = new GomokuBoard();
-            AiPlayer a = new AiPlayer("p1", "X", b, 100);
-            AiPlayer c = new AiPlayer("p2", "o", b, 100);
-            a.setup(c);
-            b.placePiece(new GamePiece(a, new Location(3,4)));
-            System.out.println(a.bid());
-        }
+        GomokuBoard b = new GomokuBoard();
+        AiPlayer a = new AiPlayer("p1", "X", b, 100);
+        AiPlayer c = new AiPlayer("p2", "o", b, 100);
+        a.setup(c);
+        b.placePiece(new GamePiece(a, new Location(3,4)));
+        System.out.println(a.bid());
     }
 }
