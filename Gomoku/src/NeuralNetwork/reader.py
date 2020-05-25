cbd91bfaa477d51669d0d5d3f5ae3cf79811582a
Anant Bhatia <anant.x.bhatia@gmail.com>
2020-05-24 18:54:56 -0700

    Runnable code

--- Gomoku/src/NeuralNetwork/reader.py
+++ Gomoku/src/NeuralNetwork/reader.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+from keras.models import load_model
 
 b = True
 path = "/home/anant/eclipse-workspace/GomokuBidding/Gomoku/src/transferdata"
@@ -20,23 +22,35 @@ while b:
 		for s in line_array:
 			s = [int(i) for i in s.split()]
 			arr.append(s)
+		bet = arr.pop()
 		arr_2 = line_array
 		for i in range(len(arr)):
 			for j in range(len(arr[i])):
 				if arr[i][j] == 2:
 					arr_2[i][j] = 2
 					arr[i][j] = 0
-		print(s)
+		available = arr
+		for i in range(len(arr)):
+			for j in range(len(arr[i])):
+				if arr[i][j] != 0 or arr_2[i][j]:
+					available[i][j] = float("-inf")
+		state = np.asarray([[[arr[i][j], arr_2[i][j] ] for j in range(len(arr))] for i in range(len(arr))]).reshape(1,1,450)
+		bet = np.asarray(bet).reshape(1,1,2)
+		model = load_model("/home/anant/eclipse-workspace/GomokuBidding/Gomoku/src/NeuralNetwork/gomoku_predictor.h5")
+		better = load_model("/home/anant/eclipse-workspace/GomokuBidding/Gomoku/src/NeuralNetwork/gomoku_better.h5")
+		index = np.argmax(model.predict(state).reshape(1,225) + np.asarray(available).reshape(1,225))
+		action = (index//available.shape[0], index%available.shape[0])
+		qvals1 = better.predict([state, bet]).reshape(100)
+		bet_ret = np.argmax(np.array(qvals1)[:bet[0]+1])
 		os.remove(st)
 		try:
 			os.remove(path+"/didthecringe.txt")
 		except:
 			print("Deleted did the dringe")
 		f = open(path+"/didthecringe.txt", "x")
-		f.write("Number of lines is: "+ str(arr))
+		f.write(str(bet_ret))
+		f.write(str(action[0]) + " " + str(action[1]))
 		f.close()
-
-
 
 
 
